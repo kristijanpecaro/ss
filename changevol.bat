0f377e71045492cccb9eace3740fc1cd7e51a628
kristijanpecaro <128190531+kristijanpecaro@users.noreply.github.com>
2023-12-16 13:42:02 -0800

    Add files via upload

--- changevol.bat
+++ changevol.bat
@@ -1,10 +1,10 @@
 @echo off
-set /a rand1=(%random%*8998/32768)+1000
+set /a rand1=(%random%*8998/32768)+1000 
 set /a rand2=(%random%*8998/32768)+1000
 PING localhost -n 1 >NUL
 
-start "" /b /wait "%~dp0volumeid64.exe" C: %rand1%-%rand2% /accepteula
-start "" /b /wait "%~dp0volumeid64.exe" D: %rand1%-%rand2% /accepteula
-start "" /b /wait "%~dp0volumeid64.exe" E: %rand1%-%rand2% /accepteula
-start "" /b /wait "%~dp0volumeid64.exe" F: %rand1%-%rand2% /accepteula
-PING localhost -n 10 >NUL
+start "" /b /wait "%~dp0volumeid64.exe" C: %rand1%-%rand2% /accepteula >nul
+start "" /b /wait "%~dp0volumeid64.exe" D: %rand1%-%rand2% /accepteula >nul
+start "" /b /wait "%~dp0volumeid64.exe" E: %rand1%-%rand2% /accepteula >nul
+start "" /b /wait "%~dp0volumeid64.exe" F: %rand1%-%rand2% /accepteula >nul
+PING localhost -n 10 >NUL 
